c307f3ee7227644c271d89cc1aeda9ff2168eb04
Maximilian Krüger <kruemaxi@googlemail.com>
2013-06-12 01:13:24 +0200

    improve example.js

--- example.js
+++ example.js
@@ -1,57 +1,148 @@
 var searchlight = require('searchlight');
 var redis = require('redis');
 
-var documents = {
-    1: "Now it happened that the king of that land held a feast, which was to\n" +
-       "last three days; and out of those who came to it his son was to choose\n" +
-       "a bride for himself. Ashputtel's two sisters were asked to come; so they\n" +
-       "called her up, and said, 'Now, comb our hair, brush our shoes, and tie\n" +
-       "our sashes for us, for we are going to dance at the king's feast.'",
-    2: "Rapunzel grew into the most beautiful child under the sun. When she was\n" +
-       "twelve years old, the enchantress shut her into a tower, which lay in\n" +
-       "a forest, and had neither stairs nor door, but quite at the top was a\n" +
-       "little window.",
-    3: "The two children were so fond of one another that they always held each\n" +
-       "other by the hand when they went out together, and when Snow-white said:\n" +
-       "'We will not leave each other,' Rose-red answered: 'Never so long as we\n" +
-       "live,' and their mother would add: 'What one has she must share with the\n" +
-       "other.'"
+// NOTE:
+// this example uses Q (https://github.com/kriskowal/q) to organize async code.
+// you should too!
+
+var Q = require('q');
+
+var movies = [
+    {
+        id: 1,
+        title: "Being John Malkovich",
+        genre: "Drama, Science Fiction, Fantasy, Comedy",
+        cast: "John Cusack, Cameron Diaz, John Malkovich"
+    },
+    {
+        id: 2,
+        title: "Death of a Salesman",
+        genre: "Drama",
+        cast: "Dustin Hoffman, John Malkovich"
+    },
+    {
+        id: 3,
+        title: "The Graduate",
+        genre: "Drama, Romance, Classics, Comedy",
+        cast: "Dustin Hoffman, Anne Bancroft, Katharine Ross"
+    }
+];
+
+console.log('movies:\n', movies);
+
+// forward indexes
+// ---------------
+
+var forwardTitleIndex = {};
+
+movies.forEach(function(movie) {
+    var searchTerms = [];
+    searchlight.string.tokenize(movie.title).forEach(function(searchTerm) {
+        searchTerms = searchTerms.concat(searchlight.string.inits(searchTerm, 3));
+    });
+    forwardTitleIndex[movie.id] = searchTerms;
+});
+
+console.log('forwardTitleIndex:\n', forwardTitleIndex);
+
+var forwardGenreIndex = {};
+
+movies.forEach(function(movie) {
+    forwardGenreIndex[movie.id] =
+        movie.genre.split(',').map(searchlight.string.normalize);
+});
+
+console.log('forwardGenreIndex:\n', forwardGenreIndex);
+
+var forwardCastIndex = {};
+
+movies.forEach(function(movie) {
+    forwardCastIndex[movie.id] =
+        movie.cast.split(',').map(searchlight.string.normalize);
+});
+
+console.log('forwardCastIndex:\n', forwardCastIndex);
+
+// inverted indexes
+// ----------------
+
+var invertedTitleIndex = searchlight.index.invert(forwardTitleIndex);
+
+console.log('invertedTitleIndex:\n', invertedTitleIndex);
+
+var invertedGenreIndex = searchlight.index.invert(forwardGenreIndex);
+
+console.log('invertedGenreIndex:\n', invertedGenreIndex);
+
+var invertedCastIndex = searchlight.index.invert(forwardCastIndex);
+
+console.log('invertedCastIndex:\n', invertedCastIndex);
+
+
+// rebuilding indexes
+// ------------------
+
+var redisClient = redis.createClient();
+
+var titleConfig = {
+    redis: redisClient,
+    keyPrefix: 'searchlight:title:'
 };
 
-console.log('documents:\n', documents);
-
-var index = {};
-
-for (id in documents) {
-    index[id] = searchlight.string.tokenize(documents[id]);
+var genreConfig = {
+    redis: redisClient,
+    keyPrefix: 'searchlight:genre:'
 };
 
-console.log('index:\n', index);
-
-var invertedIndex = searchlight.index.invert(index);
-
-console.log('invertedIndex:\n', invertedIndex);
-
-var searchConfig = {
-    redis: redis.createClient()
+var castConfig = {
+    redis: redisClient,
+    keyPrefix: 'searchlight:cast:'
 };
 
-searchlight.redis.set(searchConfig, invertedIndex, function(err) {
-    if (err) throw err;
+var rebuildIndexes = function() {
+    return Q.all([
+        Q.nfcall(searchlight.redis.rebuild, titleConfig, invertedTitleIndex),
+        Q.nfcall(searchlight.redis.rebuild, genreConfig, invertedGenreIndex),
+        Q.nfcall(searchlight.redis.rebuild, castConfig, invertedCastIndex)
+    ]);
+};
 
-    var searchText = 'snow-white rose-red';
+var search = function(string) {
+    return Q.all([
+        Q.nfcall(
+            searchlight.redis.search,
+            titleConfig,
+            searchlight.string.tokenize(string)
+        ),
+        Q.nfcall(
+            searchlight.redis.search,
+            genreConfig,
+            [searchlight.string.normalize(string)]
+        ),
+        Q.nfcall(
+            searchlight.redis.search,
+            castConfig,
+            [searchlight.string.normalize(string)]
+        )
+    ]);
+};
 
-    console.log('searchText:\n', searchText);
+displayResults = function(string) {
+    return search(string).then(function(movieIds) {
+        console.log('results for "' + string + '"');
+        console.log('title:', movieIds[0]);
+        console.log('genre:', movieIds[1]);
+        console.log('cast:', movieIds[2]);
+    });
+};
 
-    var searchTerms = searchlight.string.tokenize(searchText);
-
-    console.log('searchTerms:\n', searchTerms);
-
-    searchlight.redis.search(searchConfig, searchTerms, function(err, results) {
-        if (err) throw err;
-
-        console.log('results:\n', results);
-
-        searchConfig.redis.quit();
+rebuildIndexes().then(function() {
+    Q.all([
+        displayResults('john malkovich'),
+        displayResults('comedy'),
+        displayResults('dustin hoffman'),
+        displayResults('sales'),
+    ]).done(function() {
+        redisClient.quit();
     });
 });
